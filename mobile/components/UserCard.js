3b6ca370d089e5dd2bb08d4715ac74af984bc472
Thadeus Ajayi <thadeus.ajayi@kudabank.com>
2022-03-01 01:46:23 +0100

    format time

--- mobile/components/UserCard.js
+++ mobile/components/UserCard.js
@@ -1,18 +1,41 @@
+import moment from "moment";
 import React from "react";
 import { View, StyleSheet, Image, Text } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    marginTop: "5px",
-    flexDirection: "row"
-  }
+    marginTop: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  image: {
+    height: 80,
+    width: 80,
+    borderRadius: 40,
+    overflow: "hidden",
+    marginRight: 20,
+  },
+  name: { fontWeight: "700", fontSize: 18, marginVertical: 5 },
+  time: { fontWeight: "600", fontSize: 16, color: "#00000070" },
 });
+
+const formatTime = (time) => moment(time).format("MMM DD hh:mm");
 
 function UserStartEndTime({ starts_at, ends_at, device_time_zone }) {
   return (
-    <Text>
-      {starts_at} - {ends_at}
+    <Text style={styles.time}>
+      {formatTime(starts_at)} - {formatTime(ends_at)}
     </Text>
   );
 }
@@ -20,8 +43,16 @@ function UserStartEndTime({ starts_at, ends_at, device_time_zone }) {
 const statusColors = { current: "#C5F6A7", upcoming: "#F4F6A7" };
 
 function UserStatus({ status }) {
-  const styles = { backgroundColor: statusColors[status] };
-  return <Text style={styles}>{status}</Text>;
+  const styles = {
+    backgroundColor: statusColors[status],
+    alignSelf: "flex-end",
+    borderRadius: 10,
+    padding: 5,
+    overflow: "hidden",
+    fontWeight: "bold",
+    marginTop: 10,
+  };
+  return <Text style={styles}>{status?.toUpperCase()}</Text>;
 }
 
 function UserInfo({
@@ -30,12 +61,12 @@ function UserInfo({
   status,
   starts_at,
   ends_at,
-  device_time_zone
+  device_time_zone,
 }) {
   return (
-    <View style={{ flexDirection: "column" }}>
-      <Text>{name}</Text>
-      <Text style={{ color: "#EFEFEF" }}>{email}</Text>
+    <View style={{ flex: 1 }}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={{ color: "#00000050" }}>{email}</Text>
       <UserStartEndTime
         starts_at={starts_at}
         ends_at={ends_at}
@@ -52,9 +83,8 @@ function UserCard({ item: { attributes } }) {
       <Image
         source={{
           uri: "http://placekitten.com/g/30/30",
-          height: "30px",
-          width: "30px"
         }}
+        style={styles.image}
       />
       <UserInfo {...attributes} />
     </View>
